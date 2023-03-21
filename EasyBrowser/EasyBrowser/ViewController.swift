3244265e732561d6d9ef0e4cfec49f8a8b74f68c
BasriKomser <101523343+BasriKomser@users.noreply.github.com>
2023-03-21 13:02:50 +0300

    Hello Project

---
+++ EasyBrowser/EasyBrowser/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  EasyBrowser
+//
+//  Created by Hasan Basri Komser on 21.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
